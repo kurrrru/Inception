125f123bfd618a1ab76954e05234ca2d2b163d69
kurrrru <kurrrru8850@gmail.com>
2026-07-23 20:30:54 +0900

    TLS化

--- srcs/requirements/bonus/watcher/cmd/watcher/main.go
+++ srcs/requirements/bonus/watcher/cmd/watcher/main.go
@@ -10,6 +10,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "watcher: ok")
 	})
-	log.Println("listening on :8082")
-	log.Fatal(http.ListenAndServe(":8082", nil))
+	log.Println("listening on :8082 (TLS)")
+	log.Fatal(http.ListenAndServeTLS(":8082", "/etc/watcher/ssl/watcher.crt", "/etc/watcher/ssl/watcher.key", nil))
 }
